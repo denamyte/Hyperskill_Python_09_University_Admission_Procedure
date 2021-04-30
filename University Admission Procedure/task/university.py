a6ca7e51ce3fafa0b1d35d6afe5d15abe617bd58
denamyte <denamyte@gmail.com>
2021-05-01 02:36:32 +0300

    2021-05-01: Stage 2/7

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,1 +1,15 @@
-print(sum(int(input()) for _ in range(3)) / 3, '\nCongratulations, you are accepted!',)
+def input_score() -> float:
+    return sum(int(input()) for _ in range(3)) / 3
+
+
+def accepted(score: float) -> bool:
+    return score >= 60.0
+
+
+def print_result(score: float):
+    print(score)
+    print('Congratulations, you are accepted!' if accepted(score)
+          else 'We regret to inform you that we will not be able to offer you admission.')
+
+
+print_result(input_score())
