f661feb3dfef6e3bd5325cceb3218dc38a2b732f
denamyte <denamyte@gmail.com>
2021-05-05 02:39:18 +0300

    2021-05-05: Stage 3/7

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,15 +1,35 @@
-def input_score() -> float:
-    return sum(int(input()) for _ in range(3)) / 3
+from typing import Tuple, List
 
 
-def accepted(score: float) -> bool:
-    return score >= 60.0
+class AdmissionScores:
+    """Input student and their grades, sort them by the grades and print the admitted ones"""
+    def __init__(self):
+        self.student_count = 0
+        self.admitted_count = 0
+        self.students: List[Tuple[str, float]] = []
+
+    def input_data(self):
+        self.student_count = int(input())
+        self.admitted_count = int(input())
+        self.students = [self.input_student() for _ in range(self.student_count)]
+
+    @staticmethod
+    def input_student() -> Tuple[str, float]:
+        raw_str = input()
+        last_space_ind = raw_str.rindex(' ')
+        return raw_str[:last_space_ind], float(raw_str[last_space_ind + 1:])
+
+    def sort(self):
+        self.students.sort(key=lambda t: (-t[1], t[0]))
+
+    def __str__(self):
+        res = ['Successful applicants:']
+        for t in self.students[:self.admitted_count]:
+            res.append(t[0])
+        return '\n'.join(res)
 
 
-def print_result(score: float):
-    print(score)
-    print('Congratulations, you are accepted!' if accepted(score)
-          else 'We regret to inform you that we will not be able to offer you admission.')
-
-
-print_result(input_score())
+ad_score = AdmissionScores()
+ad_score.input_data()
+ad_score.sort()
+print(ad_score)
