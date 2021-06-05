894016add157f1f8dee6da061877c453686cf589
denamyte <denamyte@gmail.com>
2021-06-05 23:37:33 +0300

    2021-06-04: Stage 5/7: finished

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -2,24 +2,25 @@ from typing import List
 
 
 class Applicant:
-    finals_reindex = {1, 1, 3, 2, 0}  # index - Distribution.dep_names index; value - finals' index
+    finals_reindex = [1, 1, 3, 2, 0]  # index - Distribution.dep_names index; value - finals' index
 
     def __init__(self, raw_data: str):
         self.name = ''
-        self.finals: List[float] = []
-        self.dep: List[int] = []
-        self.assigned_dep = -1
+        self.finals: List[float] = []  # The order and number of finals fit Distribution.dep_names
+        self.dep_preferences: List[int] = []  # Uses indices from Distribution.dep_names
+        self.assigned_dep = -1  # Gets assigned an index from Distribution.dep_names
         self.parse(raw_data)
 
     def parse(self, raw_data: str):
         ar_data = raw_data.split(' ')
         self.name = ' '.join(ar_data[:2])
         self.finals = self.parse_finals(ar_data)
-        self.dep = [Distribution.dep_names.index(dep_name) for dep_name in ar_data[6:]]
+        self.dep_preferences = [Distribution.dep_names.index(dep_name) for dep_name in ar_data[6:]]
 
-    def parse_finals(self, ar_data: List[str]):
+    @staticmethod
+    def parse_finals(ar_data: List[str]):
         raw_finals = [float(x) for x in ar_data[2:6]]
-        return [raw_finals[i] for i in self.finals_reindex]
+        return [raw_finals[i] for i in Applicant.finals_reindex]
 
     def assign_dep(self, dep_ind: int):
         """
@@ -35,7 +36,7 @@ class Applicant:
 class Distribution:
     """
     Input applicant list, assign them into the departments given, according
-    to an applicant's preference and GPA score
+    to an applicant's preference and finals scores
     """
     dep_names = ['Biotech', 'Chemistry', 'Engineering', 'Mathematics', 'Physics']
     file_name = 'applicants.txt'
@@ -52,23 +53,22 @@ class Distribution:
             self.applicants = [Applicant(line.rstrip('\n')) for line in file.readlines()]
 
     def distribute(self):
-        # todo: Change this method; it should consider applicant.finals as the first sorting key
-
-        for level in range(3):
+        for dep_preference_level in range(3):
             for dep_ind in range(len(self.dep_names)):
                 dep_list = self.dep_lists[dep_ind]
                 if len(dep_list) < self.dep_capacity:
-                    app_list = [app for app in self.applicants if app.assigned_dep == -1 and app.dep[level] == dep_ind]
-                    self.sort_app_list(app_list)
+                    app_list = [app for app in self.applicants
+                                if app.assigned_dep == -1 and app.dep_preferences[dep_preference_level] == dep_ind]
+                    self.sort_app_list(app_list, dep_ind)
                     add_count = min(len(app_list), self.dep_capacity - len(dep_list))
                     for app in app_list[:add_count]:
                         app.assign_dep(dep_ind)
                         dep_list.append(app)
-                    self.sort_app_list(dep_list)
+                    self.sort_app_list(dep_list, dep_ind)
 
     @staticmethod
-    def sort_app_list(app_list: List[Applicant]):
-        app_list.sort(key=lambda app: (-app.gpa, app.name))
+    def sort_app_list(app_list: List[Applicant], dep_ind: int):
+        app_list.sort(key=lambda app: (-app.finals[dep_ind], app.name))
 
     def dep_to_str(self, dep_ind: int):
         app_str = '\n'.join(map(str, self.dep_lists[dep_ind]))
